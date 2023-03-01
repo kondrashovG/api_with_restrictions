f56ffba573570559ef2ba6ecd9ede2fd71d6f754
kondrashovG <kondrashovg@gmail.com>
2023-03-01 23:49:10 +0300

    готово

---
+++ advertisements/serializers.py
@@ -0,0 +1,45 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from advertisements.models import Advertisement
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer для пользователя."""
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name',)
+
+
+class AdvertisementSerializer(serializers.ModelSerializer):
+    """Serializer для объявления."""
+
+    creator = UserSerializer(
+        read_only=True,
+    )
+
+    class Meta:
+        model = Advertisement
+        fields = ('id', 'title', 'description', 'creator',
+                  'status', 'created_at', 'updated_at', )
+
+    def create(self, validated_data):
+        """Метод для создания"""
+
+        # Простановка значения поля создатель по-умолчанию.
+        # Текущий пользователь является создателем объявления
+        # изменить или переопределить его через API нельзя.
+        # обратите внимание на `context` – он выставляется автоматически
+        # через методы ViewSet.
+        # само поле при этом объявляется как `read_only=True`
+        validated_data["creator"] = self.context["request"].user
+        return super().create(validated_data)
+
+    def validate(self, data):
+        """Метод для валидации. Вызывается при создании и обновлении."""
+
+        num_adv = Advertisement.objects.filter(status='OPEN', creator=self.context["request"].user).count()
+        if self.initial_data.get('status') in ('OPEN', None) and num_adv > 9:
+            raise ValidationError(f'У вас исчерпан лимит открытых объявлений: 10')
+        return data
---
+++ advertisements/views.py
@@ -0,0 +1,29 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from advertisements.filters import AdvertisementFilter
+from advertisements.models import Advertisement
+from advertisements.permissions import IsOwnerOrReadOnly
+from advertisements.serializers import AdvertisementSerializer
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    filter_backends = [DjangoFilterBackend, ]
+    filterset_fields = ['creator', 'status']
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            return [IsOwnerOrReadOnly()]
+        return []
+
+    def list(self, request, **kwargs):
+        queryset = Advertisement.objects.all()
+        queryset = AdvertisementFilter(data=request.GET, queryset=queryset, request=request).qs
+        serializer = AdvertisementSerializer(queryset, many=True)
+        return Response(serializer.data)
